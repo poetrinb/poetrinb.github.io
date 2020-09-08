21c6bd8afb7f7502c3164879c155d9d7465a69d6
badrun <bbadrunn@gmail.com>
2020-09-08 09:47:36 +0700

    menanbahkan beberapa animasi baru

--- js/script.js
+++ js/script.js
@@ -120,3 +120,18 @@ function rotator() {
 } 
 
 setInterval(rotator, 1000);
+
+const deg = 6;
+const mn = document.querySelector("#mn");
+const hr = document.querySelector("#hr");
+const sc = document.querySelector("#sc");
+
+setInterval(() => {
+	let day = new Date();
+	let hh = day.getHours() * 30;
+	let mm = day.getMinutes() * deg;
+	let ss = day.getSeconds() * deg;
+	hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+	mn.style.transform = `rotateZ(${mm}deg)`;
+	sc.style.transform = `rotateZ(${ss}deg)`;
+});
